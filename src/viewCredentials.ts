6f81fe5ae7f61d6628f0ced8301cf974c6c4d204
Waleed Gudah <aquataze@yahoo.com>
2024-03-30 01:35:54 -0500

    Refactor `viewCredentials` for improved readability and performance

--- src/viewCredentials.ts
+++ src/viewCredentials.ts
@@ -3,19 +3,17 @@ import { CredentialManager } from "./CredentialManager";
 async function viewCredentials() {
   const credentialManager = new CredentialManager();
 
-  // Ensure database initialization is complete before listing credentials
   await credentialManager.ensureDBInit();
 
-  // Now it's safe to proceed with database operations
   const result = await credentialManager.getAllCredentials();
 
   if (result.status) {
     if (result.count > 0) {
       console.log(`${result.message} Total credentials: ${result.count}`);
       result.credentials.forEach((cred, index) => {
-        console.log(`Credential #${index + 1}:`);
         console.log(`Name: ${cred.name}`);
-        console.log('Keys:', cred.keys); // Assuming 'keys' is an array you want to print out directly
+        console.log('Keys:', cred.keys); 
+        console.log(`-----`);
       });
     } else {
       console.log("No credentials found.");
