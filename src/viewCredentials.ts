af9bc47adc48b886e3f99474d4eecea34eeaab57
Waleed Gudah <aquataze@yahoo.com>
2024-03-30 02:19:13 -0500

    Refactor src/viewCredentials.ts for improved readability and performance

--- src/viewCredentials.ts
+++ src/viewCredentials.ts
@@ -7,35 +7,30 @@ async function viewCredentials() {
 
   const result = await credentialManager.getAllCredentials();
 
+  console.log(`\nCREDENTIALS & KEYS: ${result.message}`);
   if (result.status) {
-
-    console.log(`${result.message}`);
-    console.log(`Total services: ${result.servicesCount}`);
-    console.log(`Total services: ${result.totalCredentials}`);
-
+    console.log(`-Total services managed: ${result.servicesCount}`);
+    console.log(`-Total credentials stored: ${result.totalCredentials}\n`);
+console.log(" Credentials:");
     result.credentials.forEach((cred) => {
-      console.log(`   -${cred.name}`);
+      console.log(` -${cred.name}`);
 
       const hasPrimary = cred.keys.some((key: { keyName: string; }) => key.keyName === 'Primary');
       const hasSecondary = cred.keys.some((key: { keyName: string; }) => key.keyName === 'Secondary');
 
       cred.keys.forEach((key: { keyName: any; apiKey: any; }) => {
-        console.log(`  ${key.keyName}: ${key.apiKey}`);
+        console.log(`   ${key.keyName}: ${key.apiKey}`);
       });
 
       if (!hasPrimary) {
-        console.log("  Primary: Add Key Now");
+        console.log("   Primary: <Add Key Now>");
       }
       if (!hasSecondary) {
-        console.log("  Secondary: Add Key Now");
+        console.log("   Secondary: <Add Key Now>");
       }
-
-      console.log("\n"); 
     });
-
-  
   } else {
-    console.log("No credentials found.");
+    console.log("   No credentials found.");
   }
 }
 
