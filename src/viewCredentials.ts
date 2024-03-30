579540451d07219dc6a91e996fcf07f7c39b4e50
Waleed Gudah <aquataze@yahoo.com>
2024-03-30 01:44:56 -0500

    Refactor src/viewCredentials.ts for enhanced readability and performance optimization

--- src/viewCredentials.ts
+++ src/viewCredentials.ts
@@ -3,17 +3,28 @@ import { CredentialManager } from "./CredentialManager";
 async function viewCredentials() {
   const credentialManager = new CredentialManager();
 
+  // Ensure database initialization is complete before listing credentials
   await credentialManager.ensureDBInit();
 
+  // Now it's safe to proceed with database operations
   const result = await credentialManager.getAllCredentials();
 
   if (result.status) {
+    console.log(`\n${result.message}`);
     if (result.count > 0) {
-      console.log(`${result.message} Total credentials: ${result.count}`);
-      result.credentials.forEach((cred, index) => {
-        console.log(`Name: ${cred.name}`);
-        console.log('Keys:', cred.keys); 
-        console.log(`-----`);
+      console.log(`Total credentials: ${result.count}\n`);
+
+      result.credentials.forEach((cred) => {
+        // Prepare a new object for each credential to hold the keys as key-value pairs
+        //@ts-ignore
+        const keysObject = cred.keys.reduce((obj:any, { keyName, apiKey }) => {
+          obj[keyName] = apiKey;
+          return obj;
+        }, {});
+
+        // Log the credential name and its keys as an object
+        console.log(`-${cred.name}`);
+        console.log(keysObject);
       });
     } else {
       console.log("No credentials found.");
