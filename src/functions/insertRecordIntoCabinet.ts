8034c1b20a35f8fb7857ecca23e78fc2424cdd3a
Waleed Gudah <aquataze@yahoo.com>
2024-04-02 11:49:59 -0500

    Add insertRecordIntoCabinet function to project

---
+++ src/functions/insertRecordIntoCabinet.ts
@@ -0,0 +1,53 @@
+import { Db } from 'mongodb';
+
+interface RecordData {
+    name: string;
+    credential: string;
+    created_at: Date;
+    updated_at: Date;
+    uid?: string; 
+    description?: string
+  }
+  
+/**
+ * Inserts a record into the specified cabinet (collection).
+ * 
+ * @param {Object} params - The function parameters.
+ * @param {Db} params.dbConnection - The MongoDB connection.
+ * @param {string} params.cabinetName - The name of the cabinet (collection) where the record will be inserted.
+ * @param {RecordData} params.recordData - The data for the record to be inserted, conforming to the RecordData interface.
+ * @returns {Promise<Object>} The result object containing the status, insertedId, and a message.
+ */
+
+export async function insertRecordIntoCabinet({
+    dbConnection,
+    cabinetName,
+    recordData,
+}: {
+    dbConnection: Db,
+    cabinetName: string,
+    recordData: RecordData,
+}): Promise<{ status: boolean; insertedId?: string; message: string }> {
+    try {
+        const cabinet = dbConnection.collection(cabinetName);
+        const result = await cabinet.insertOne(recordData);
+
+        if (result.insertedId) {
+            return {
+                status: true,
+                insertedId: result.insertedId.toString(), // Convert ObjectId to string for easy handling
+                message: `Record was successfully inserted into cabinet '${cabinetName}' with ID: ${result.insertedId}.`,
+            };
+        } else {
+            return {
+                status: false,
+                message: "Failed to insert the record into the cabinet.",
+            };
+        }
+    } catch (error: any) {
+        return {
+            status: false,
+            message: `Failed to insert the record into the cabinet: ${error.message}`,
+        };
+    }
+}
