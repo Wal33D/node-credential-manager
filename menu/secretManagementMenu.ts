e9437f8726b7319ed96c07edd99787c72d52723f
Waleed Gudah <aquataze@yahoo.com>
2024-04-06 06:06:42 -0500

    Refactored secretManagementMenu.ts for improved performance

--- menu/secretManagementMenu.ts
+++ menu/secretManagementMenu.ts
@@ -1,31 +1,33 @@
 import ReadlineManager from "../utils/ReadlineManager";
-import { secrets } from "../src/database/secrets"; // Adjust the path as necessary
+import { secrets } from "../src/database/secrets"; 
 
-const performSecretAction = async (dbClient:any, action:any, mainMenuCallback:any) => {
+const performSecretAction = async (dbClient: any, action: any, mainMenuCallback: any) => {
     let secretName = '', newSecretName = '';
     const projectName = await ReadlineManager.askQuestion('Enter project name: ');
     const serviceName = await ReadlineManager.askQuestion('Enter service name: ');
+    let decrypted = false;
     if (action === 'rename') {
         secretName = await ReadlineManager.askQuestion('Enter current secret name: ') as string;
         newSecretName = await ReadlineManager.askQuestion('Enter new secret name: ') as string;
     } else if (['add', 'remove', 'exists', 'findByName'].includes(action)) {
-        
+
         secretName = await ReadlineManager.askQuestion('Enter secret name: ') as any;
     }
-
+    if (['findByName', 'find', 'list'].includes(action)) {
+        decrypted = (await ReadlineManager.askQuestion('Do you want to decrypt the secret? (yes/no)`') === 'yes');
+    }
     try {
         //@ts-ignore
-        const response = await secrets[action]({ dbClient, projectName, serviceName, secretName, newSecretName });
+        const response = await secrets[action]({ dbClient, projectName, serviceName, secretName, newSecretName, decrypted });
         console.log(`${action.charAt(0).toUpperCase() + action.slice(1)} Secret Response:`, JSON.stringify(response, null, 2));
-    } catch (error:any) {
+    } catch (error: any) {
         console.error(`An error occurred during ${action}:`, error.message);
     }
 
-    // Return to the secret management menu after action completion
     await secretManagementMenu(dbClient, mainMenuCallback);
 };
 
-export const secretManagementMenu = async (dbClient:any, mainMenuCallback:any) => {
+export const secretManagementMenu = async (dbClient: any, mainMenuCallback: any) => {
     console.log('\nSecret Management Menu:');
     console.log('1. List Secrets');
     console.log('2. Add Secret');
@@ -37,21 +39,21 @@ export const secretManagementMenu = async (dbClient:any, mainMenuCallback:any) =
 
     const choice = await ReadlineManager.askQuestion('Enter your choice: ');
 
-    switch(choice) {
+    switch (choice) {
         case '1':
             await performSecretAction(dbClient, 'list', mainMenuCallback);
             break;
         case '2':
-            await performSecretAction(dbClient, 'add',mainMenuCallback);
+            await performSecretAction(dbClient, 'add', mainMenuCallback);
             break;
         case '3':
             await performSecretAction(dbClient, 'rename', mainMenuCallback);
             break;
         case '4':
-            await performSecretAction(dbClient, 'remove',  mainMenuCallback);
+            await performSecretAction(dbClient, 'remove', mainMenuCallback);
             break;
         case '5':
-            await performSecretAction(dbClient, 'exists',mainMenuCallback);
+            await performSecretAction(dbClient, 'exists', mainMenuCallback);
             break;
         case '6':
             await performSecretAction(dbClient, 'findByName', mainMenuCallback);
