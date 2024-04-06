9fddcfbbe335adeb21375b89f5d9d595d191a459
Waleed Gudah <aquataze@yahoo.com>
2024-04-06 02:30:49 -0500

    Refactor startMenu and update encryptionInit; add new projectManagementMenu module

--- src/projectManagementMenu.ts
+++ src/projectManagementMenu.ts
@@ -1,9 +1,5 @@
 import readline from "readline";
-import { runAllTests } from "./tests/runAllTests";
-import { initializeDbConnection } from "./database/initializeDbConnection";
-import { checkAndGenerateEncryptionKey } from "./utils/encryptionInit";
 import { projects } from "./database/projects";
-import { projectManagementMenu } from "./projectManagement";
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -18,26 +14,13 @@ const askQuestion = (query:any) => {
     });
 };
 
-const startApplication = async () => {
-    console.log("Initializing database connection...");
-
-    const connectionResult = await initializeDbConnection({}); 
-    if (!connectionResult.status) {
-        console.error("Failed to initialize database connection:", connectionResult.message);
-        process.exit(1);
-    }
-
-    const dbClient = connectionResult.client;
-    await mainMenu(dbClient);
-};
-
-const listProjects = async (dbClient: any) => {
+const listProjects = async (dbClient:any) => {
     const response = await projects.list({ dbClient });
     console.log('Projects List:', JSON.stringify(response, null, 2));
-    mainMenu(dbClient);
+    projectManagementMenu(dbClient);
 };
 
-const createProject = async (dbClient: any) => {
+const createProject = async (dbClient:any) => {
     const projectName = await askQuestion('Enter project name: ') as string;
     const serviceName = await askQuestion('Enter the first service\'s name: ') as string;
     const response = await projects.create({
@@ -46,21 +29,20 @@ const createProject = async (dbClient: any) => {
         serviceName
     });
     console.log('Create Project Response:', JSON.stringify(response, null, 2));
-    mainMenu(dbClient);
+    projectManagementMenu(dbClient);
 };
 
-
-const deleteProject = async (dbClient: any) => {
+const deleteProject = async (dbClient:any) => {
     const projectName = await askQuestion('Enter project name to delete: ') as string;
     const response = await projects.delete({
         dbClient,
         projectName
     });
     console.log('Delete Project Response:', JSON.stringify(response, null, 2));
-    mainMenu(dbClient);
+    projectManagementMenu(dbClient);
 };
 
-const copyProject = async (dbClient: any) => {
+const copyProject = async (dbClient:any) => {
     const sourceProjectName = await askQuestion('Enter source project name: ') as string;
     const targetProjectName = await askQuestion('Enter target project name: ') as string;
     const response = await projects.copy({
@@ -69,59 +51,20 @@ const copyProject = async (dbClient: any) => {
         targetProjectName
     });
     console.log('Copy Project Response:', JSON.stringify(response, null, 2));
-    mainMenu(dbClient);
+    projectManagementMenu(dbClient);
 };
 
-const checkProjectExists = async (dbClient: any) => {
-    const projectName = await askQuestion('Enter project name to check: ') as any;
+const checkProjectExists = async (dbClient:any) => {
+    const projectName = await askQuestion('Enter project name to check: ') as string;
     const response = await projects.exists({
         dbClient,
         projectName
     });
     console.log('Check Project Exists Response:', JSON.stringify(response, null, 2));
-    mainMenu(dbClient);
+    projectManagementMenu(dbClient);
 };
 
-const mainMenu = async (dbClient: any) => {
-    console.log('\nMain Menu:');
-    console.log('1. Project Management');
-    console.log('2. Run All Tests');
-    console.log('3. Check and Generate Encryption Key');
-    console.log('3. Other functionality here...');
-    console.log('4. Exit');
-
-    const choice = await askQuestion('Enter your choice: ');
-
-    switch (choice) {
-        case '1':
-            await projectManagementMenu(dbClient);
-            break;
-        case '2':
-            console.log('Running all tests...');
-            await runAllTests();
-            console.log('Tests completed.');
-            await mainMenu(dbClient);
-            break;
-        case '3':
-            console.log('Checking and generating encryption key...');
-            await checkAndGenerateEncryptionKey();
-            console.log('Operation completed.');
-            await mainMenu(dbClient);
-            break;
-        case '4':
-            console.log('Exiting application...');
-            rl.close();
-            if (dbClient && dbClient.close) {
-                dbClient.close();
-            }
-            break;
-        default:
-            console.log('Invalid choice. Please select a valid option.');
-            await mainMenu(dbClient);
-    }
-};
-
-const projectManagementMenu = async (dbClient: any) => {
+const projectManagementMenu = async (dbClient:any) => {
     console.log('\nProject Management Menu:');
     console.log('1. List Projects');
     console.log('2. Create Project');
@@ -149,16 +92,12 @@ const projectManagementMenu = async (dbClient: any) => {
             await checkProjectExists(dbClient);
             break;
         case '6':
-            await mainMenu(dbClient);
+            // Return to the main menu needs to be handled outside this module
             break;
         default:
             console.log('Invalid choice. Please select a valid option.');
             await projectManagementMenu(dbClient); 
     }
-
-    mainMenu(dbClient);
-
 };
 
-startApplication();
-
+export { projectManagementMenu };
