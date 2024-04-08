8b9e0082830b779571c0e5bd0a9adb130ac1f760
Waleed Gudah <aquataze@yahoo.com>
2024-04-08 01:05:15 -0500

    Refactor secret management and remove deprecated env file generation function

--- menu/secretManagementMenu.ts
+++ menu/secretManagementMenu.ts
@@ -1,8 +1,8 @@
 import ReadlineManager from "../utils/ReadlineManager";
-import { secrets } from "../src/database/secrets"; 
+import { secrets } from "../src/database/secrets";
 
 const performSecretAction = async (dbClient: any, action: any, mainMenuCallback: any) => {
-    let secretName = '', newSecretName = '';
+    let secretName = '', newSecretName = '', outputPath = '';
     const projectName = await ReadlineManager.askQuestion('Enter project name: ');
     const serviceName = await ReadlineManager.askQuestion('Enter service name: ');
     let decrypted = false;
@@ -10,15 +10,22 @@ const performSecretAction = async (dbClient: any, action: any, mainMenuCallback:
         secretName = await ReadlineManager.askQuestion('Enter current secret name: ') as string;
         newSecretName = await ReadlineManager.askQuestion('Enter new secret name: ') as string;
     } else if (['add', 'remove', 'exists', 'findByName'].includes(action)) {
-
         secretName = await ReadlineManager.askQuestion('Enter secret name: ') as any;
     }
-    if (['findByName', 'find', 'list'].includes(action)) {
-        decrypted = (await ReadlineManager.askQuestion('Do you want to decrypt the secret? (yes/no)`') === 'yes');
+    if (['findByName', 'find', 'list', 'export'].includes(action)) {
+        decrypted = (await ReadlineManager.askQuestion('Do you want to decrypt the secret? (yes/no): ') === 'yes');
     }
+    if (action === 'export') {
+        outputPath = await ReadlineManager.askQuestion('Enter the output path for the .env file: ') as string;
+    }
+
     try {
-        //@ts-ignore
-        const response = await secrets[action]({ dbClient, projectName, serviceName, secretName, newSecretName, decrypted });
+        const response = action === 'export'
+            //@ts-ignore
+            ? await secrets[action]({ dbClient, projectName, serviceName, outputPath, decrypted })
+            //@ts-ignore
+            : await secrets[action]({ dbClient, projectName, serviceName, secretName, newSecretName, decrypted });
+
         console.log(`${action.charAt(0).toUpperCase() + action.slice(1)} Secret Response:`, JSON.stringify(response, null, 2));
     } catch (error: any) {
         console.error(`An error occurred during ${action}:`, error.message);
@@ -35,7 +42,8 @@ export const secretManagementMenu = async (dbClient: any, mainMenuCallback: any)
     console.log('4. Remove Secret');
     console.log('5. Check if Secret Exists');
     console.log('6. Get Secret');
-    console.log('7. Return to Main Menu');
+    console.log('7. Export Secrets to .env');
+    console.log('8. Return to Main Menu');
 
     const choice = await ReadlineManager.askQuestion('Enter your choice: ');
 
@@ -59,6 +67,9 @@ export const secretManagementMenu = async (dbClient: any, mainMenuCallback: any)
             await performSecretAction(dbClient, 'findByName', mainMenuCallback);
             break;
         case '7':
+            await performSecretAction(dbClient, 'export', mainMenuCallback);
+            break;
+        case '8':
             await mainMenuCallback(dbClient);
             break;
         default:
